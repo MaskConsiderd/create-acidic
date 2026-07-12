75fc0d2b94239279a499740c60baff9d919e5c87
MaskConsidered <masked404.lol@gmail.com>
2026-07-12 15:30:43 -0500

    adjusting placements/size

--- src/main/java/net/masked/createacidic/client/overlay/ScienceGogglesOverlayRenderer.java
+++ src/main/java/net/masked/createacidic/client/overlay/ScienceGogglesOverlayRenderer.java
@@ -58,6 +58,9 @@ public class ScienceGogglesOverlayRenderer {
 
         hoverTicks++;
 
+        int iconSize = 16;
+        int iconPadding = 4;
+
         int textWidth = 0;
         for (Component line : tooltip)
             textWidth = Math.max(textWidth, mc.font.width(line));
@@ -65,10 +68,15 @@ public class ScienceGogglesOverlayRenderer {
         int lineHeight = 10;
         int textHeight = 8 + (tooltip.size() > 1 ? 2 + (tooltip.size() - 1) * lineHeight : 0);
 
-        int posX = width / 2 + 20;
-        int posY = height / 2 - 30;
-        posX = Math.min(posX, width - textWidth - 24);
-        posY = Math.min(posY, height - textHeight - 20);
+        int panelWidth = iconSize + iconPadding + textWidth;
+        int panelHeight = Math.max(textHeight, iconSize);
+
+        int posX = width / 2 + 50;
+        int posY = height / 2 - 10;
+        posX = Math.min(posX, width - panelWidth - 24);
+        posY = Math.min(posY, height - panelHeight - 20);
+
+        int textStartX = posX + iconSize + iconPadding;
 
         float fade = net.minecraft.util.Mth.clamp((hoverTicks + partialTicks) / 24f, 0f, 1f);
 
@@ -80,20 +88,20 @@ public class ScienceGogglesOverlayRenderer {
         int borderTop = ((int) (0x50 * fade) << 24) | 0x5000FF;
         int borderBot = ((int) (0x28 * fade) << 24) | 0x5000FF;
 
-        graphics.fillGradient(posX - 4, posY - 4, posX + textWidth + 4, posY + textHeight + 4, background, background);
-        graphics.fillGradient(posX - 4, posY - 4, posX + textWidth + 4, posY - 3, borderTop, borderTop);
-        graphics.fillGradient(posX - 4, posY + textHeight + 3, posX + textWidth + 4, posY + textHeight + 4, borderBot, borderBot);
-        graphics.fillGradient(posX - 4, posY - 3, posX - 3, posY + textHeight + 3, borderTop, borderBot);
-        graphics.fillGradient(posX + textWidth + 3, posY - 3, posX + textWidth + 4, posY + textHeight + 3, borderTop, borderBot);
+        graphics.fillGradient(posX - 4, posY - 4, posX + panelWidth + 4, posY + panelHeight + 4, background, background);
+        graphics.fillGradient(posX - 4, posY - 4, posX + panelWidth + 4, posY - 3, borderTop, borderTop);
+        graphics.fillGradient(posX - 4, posY + panelHeight + 3, posX + panelWidth + 4, posY + panelHeight + 4, borderBot, borderBot);
+        graphics.fillGradient(posX - 4, posY - 3, posX - 3, posY + panelHeight + 3, borderTop, borderBot);
+        graphics.fillGradient(posX + panelWidth + 3, posY - 3, posX + panelWidth + 4, posY + panelHeight + 3, borderTop, borderBot);
 
         ItemStack icon = new ItemStack(net.masked.createacidic.registry.ModItems.SCIENCE_GOGGLES.get());
-        graphics.renderItem(icon, posX - 20, posY - 2);
+        graphics.renderItem(icon, posX, posY - 1);
 
         int textAlpha = (int) (0xFF * fade) << 24;
         int y = posY;
         for (int i = 0; i < tooltip.size(); i++) {
             Component line = tooltip.get(i);
-            graphics.drawString(mc.font, line, posX, y, 0xFFFFFF | textAlpha, true);
+            graphics.drawString(mc.font, line, textStartX, y, 0xFFFFFF | textAlpha, true);
             y += lineHeight;
             if (i == 0 && tooltip.size() > 1)
                 y += 2;
