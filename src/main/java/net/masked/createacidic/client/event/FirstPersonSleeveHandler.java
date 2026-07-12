da88cc1958570eb10d75721e604e67b5505612bc
MaskConsidered <masked404.lol@gmail.com>
2026-07-12 16:46:00 -0500

    still trying to fix sleeve position

--- src/main/java/net/masked/createacidic/client/event/FirstPersonSleeveHandler.java
+++ src/main/java/net/masked/createacidic/client/event/FirstPersonSleeveHandler.java
@@ -28,7 +28,7 @@ public class FirstPersonSleeveHandler {
                 .getBuffer(RenderType.entityCutoutNoCull(SLEEVE_TEXTURE));
 
         event.getPoseStack().pushPose();
-        event.getPoseStack().translate(0.0, -6.0 / 16.0, 0.0); // tune this value empirically
+        event.getPoseStack().translate(0.0, 10.0 / 16.0, 0.0); // tune this value empirically
 
         LabCoatSleeveModel.render(event.getPoseStack(), buffer, event.getPackedLight(), event.getArm());
 
